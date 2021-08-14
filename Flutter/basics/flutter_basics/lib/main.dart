347ef6f5f4c330bc18f7b6bcc6c1a6f136566313
Tara Barghian <ta94ra12@gmail.com>
2021-08-14 23:19:50 +0430

    playing with flutter

--- Flutter/basics/flutter_basics/lib/main.dart
+++ Flutter/basics/flutter_basics/lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
-// void main(List<String> args) {
-//   runApp(MyApp());
-// }
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void answerQestion() {
+    setState(() {
+      if (_questionIndex >= 1) {
+        _questionIndex = 0;
+      } else {
+        _questionIndex++;
+      }
+      print('hihihi');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = ['what\'s your fav color?', 'what\'s your fav animal?'];
@@ -18,27 +34,14 @@ class MyApp extends StatelessWidget {
             ),
             body: Column(
               children: [
-                Text('The questions!'),
-                RaisedButton(
-                    child: Text('Answer 1'),
-                    onPressed: () {
-                      print('Answer 1 chosen');
-                    }),
-                RaisedButton(
-                  child: Text('Answer 2'),
-                  onPressed: () => print('Answer 2 chosen'),
-                ),
-                RaisedButton(
-                  child: Text('Answer 3'),
-                  onPressed: answerQestion,
-                ),
+                Text(questions.elementAt(_questionIndex)),
+                RaisedButton(child: Text('Answer 1'), onPressed: answerQestion),
+                RaisedButton(child: Text('Answer 2'), onPressed: answerQestion),
+                RaisedButton(child: Text('Answer 3'), onPressed: answerQestion),
               ],
             )));
   }
 
-  void answerQestion() {
-    print('Answer 3 chosen');
-  }
   // ctrl + space -> auto complete
 
 }
