5327c24ec863708547b8c98a4a0d68dd55fcc764
Tara Barghian <ta94ra12@gmail.com>
2021-08-14 21:53:25 +0430

    playing with flutter

--- Flutter/basics/flutter_basics/lib/main.dart
+++ Flutter/basics/flutter_basics/lib/main.dart
@@ -9,8 +9,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = ['what\'s your fav color?', 'what\'s your fav animal?'];
+
     return MaterialApp(
-      home: Text('Hello ^_^'),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Quiz!'),
+            ),
+            body: Column(
+              children: [
+                Text('The questions!'),
+                RaisedButton(
+                    child: Text('Answer 1'),
+                    onPressed: () {
+                      print('Answer 1 chosen');
+                    }),
+                RaisedButton(
+                  child: Text('Answer 2'),
+                  onPressed: () => print('Answer 2 chosen'),
+                ),
+                RaisedButton(
+                  child: Text('Answer 3'),
+                  onPressed: answerQestion,
+                ),
+              ],
+            )));
   }
+
+  void answerQestion() {
+    print('Answer 3 chosen');
+  }
+  // ctrl + space -> auto complete
+
 }
